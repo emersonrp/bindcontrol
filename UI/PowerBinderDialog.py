75957faccae0c91e864c8d289f0294632ad48863
Russell Pickett <emerson@hayseed.net>
2021-06-10 01:45:51 -0400

    More work on sub-UI bits

--- UI/PowerBinderDialog.py
+++ UI/PowerBinderDialog.py
@@ -52,23 +52,55 @@ class PowerBinderDialog(wx.Dialog):
         autoPowerSizer.Add(autoPowerName, 1, wx.ALIGN_CENTER_VERTICAL)
         sizer.Add(autoPowerSizer, 0, wx.EXPAND|wx.TOP, 15)
         sizer.Hide(autoPowerSizer)
-
         self.ExtraUI[autoPowerIndex] = { UI: autoPowerSizer }
 
         ####### Chat Command
         chatCommandIndex = self.bindChoice.FindString("Chat Command")
-        chatCommandName = wx.TextCtrl(self, -1)
-        sizer.Add(chatCommandName, 0, wx.EXPAND|wx.TOP, 15)
-        sizer.Hide(chatCommandName)
-
-        self.ExtraUI[chatCommandIndex] = { UI: chatCommandName }
+        chatCommandSizer = wx.GridBagSizer(5, 5)
+        chatCommandUseColorsCB = wx.CheckBox(self, -1, "Use Chat Bubble Colors")
+        chatCommandSizer.Add(chatCommandUseColorsCB, (0,0), (1,6), flag=wx.ALIGN_CENTER_VERTICAL)
+        # row 1
+        chatCommandBorderColor = wx.ColourPickerCtrl(self, -1)
+        chatCommandSizer.Add(wx.StaticText(self, -1, "Border:"), (1,0), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        chatCommandSizer.Add(chatCommandBorderColor, (1,1))
+        chatCommandBGColor = wx.ColourPickerCtrl(self, -1)
+        chatCommandSizer.Add(wx.StaticText(self, -1, "Background:"), (1,2), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        chatCommandSizer.Add(chatCommandBGColor, (1,3))
+        chatCommandFGColor = wx.ColourPickerCtrl(self, -1)
+        chatCommandSizer.Add(wx.StaticText(self, -1, "Text:"), (1,4), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        chatCommandSizer.Add(chatCommandFGColor, (1,5))
+        # row 2
+        chatCommandDuration = wx.SpinCtrl(self, -1, style=wx.SP_ARROW_KEYS)
+        chatCommandDuration.SetRange(1, 20)
+        chatCommandDuration.SetValue(7)
+        chatCommandSizer.Add(wx.StaticText(self, -1, "Duration:"), (2,0), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        chatCommandSizer.Add(chatCommandDuration, (2,1))
+        chatCommandChatSize = wx.Choice(self, -1,
+               choices = ['0.5', '0.6', '0.7', '0.8', '0.9', '1', '1.1', '1.2', '1.3', '1.4', '1.5'])
+        chatCommandChatSize.SetSelection(5)
+        chatCommandSizer.Add(wx.StaticText(self, -1, "Size:"), (2,2), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        chatCommandSizer.Add(chatCommandChatSize, (2,3))
+        chatCommandChannel = wx.Choice(self, -1,
+               choices = ['say', 'group', 'broadcast', 'local', 'yell', 'friends', 'general',
+                        'request', 'arena', 'supergroup', 'coalition', 'tell $target,', 'tell $name,'])
+        chatCommandChannel.SetSelection(0)
+        chatCommandSizer.Add(wx.StaticText(self, -1, "Channel:"), (2,4), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+        chatCommandSizer.Add(chatCommandChannel, (2,5))
+        # row 3
+        chatCommandUseColorsCB = wx.CheckBox(self, -1, "Use Beginchat")
+        chatCommandSizer.Add(chatCommandUseColorsCB, (3,0), (1,2), flag=wx.ALIGN_CENTER_VERTICAL)
+        chatCommandMessage = wx.TextCtrl(self, -1)
+        chatCommandMessage.SetHint('Message')
+        chatCommandSizer.Add(chatCommandMessage, (3,2), (1,4), flag=wx.EXPAND)
+        sizer.Add(chatCommandSizer, 0, wx.EXPAND|wx.TOP, 15)
+        # sizer.Hide(chatCommandSizer)
+        self.ExtraUI[chatCommandIndex] = { UI: chatCommandSizer }
 
         ####### Chat Command Global
         chatCommandGlobalIndex = self.bindChoice.FindString("Chat Command")
         chatCommandGlobalName = wx.TextCtrl(self, -1)
         sizer.Add(chatCommandGlobalName, 0, wx.EXPAND|wx.TOP, 15)
         sizer.Hide(chatCommandGlobalName)
-
         self.ExtraUI[chatCommandGlobalIndex] = { UI: chatCommandGlobalName }
 
         #######Costume Change
@@ -81,7 +113,6 @@ class PowerBinderDialog(wx.Dialog):
         costumeChangeSizer.Add(costumeChangeCostume, 1, wx.ALIGN_CENTER_VERTICAL)
         sizer.Add(costumeChangeSizer, 0, wx.EXPAND|wx.TOP, 15)
         sizer.Hide(costumeChangeSizer)
-
         self.ExtraUI[costumeChangeIndex] = { UI: costumeChangeSizer }
 
 

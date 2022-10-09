afc918b6866af34c402d71db51e49f1c304c605c
Russell Pickett <emerson@hayseed.net>
2022-10-09 16:35:05 -0400

    Support icons for powers

--- UI/PowerPicker.py
+++ UI/PowerPicker.py
@@ -1,5 +1,6 @@
 import wx
 import GameData
+from Utility import Icon
 
 class PowerPicker(wx.Button):
     def __init__(self, parent):
@@ -30,12 +31,16 @@ class PowerPickerMenu(wx.Menu):
         archdata = GameData.Archetypes[gen.GetState('Archetype')]
         for category in ['Primary', 'Secondary', 'Epic']:
             submenu = wx.Menu()
-            catname = gen.GetState(category)
-            if not catname: continue
-            self.AppendSubMenu(submenu, f"{category}: {catname}")
+            powerset = gen.GetState(category)
+            if not powerset: continue
+            self.AppendSubMenu(submenu, f"{category}: {powerset}")
 
-            powers = archdata[category][catname]
-            for power in powers: submenu.Append(-1, power)
+            powers = archdata[category][powerset]
+            for power in powers:
+                menuitem = wx.MenuItem(id = wx.ID_ANY, text = power)
+                icon = Icon(powerset = powerset, power = power)
+                if icon: menuitem.SetBitmap(icon)
+                submenu.Append(menuitem)
 
         # Pool powers
         for poolpicker in ["Pool1", "Pool2", "Pool3", "Pool4"]:
@@ -45,7 +50,11 @@ class PowerPickerMenu(wx.Menu):
                 self.AppendSubMenu(submenu, "Pool: " + poolname)
 
                 powers = GameData.MiscPowers['Pool'][poolname]
-                for power in powers: submenu.Append(-1, power)
+                for power in powers:
+                    menuitem = wx.MenuItem(id = wx.ID_ANY, text = power)
+                    icon = Icon(powerset = poolname, power = power)
+                    if icon: menuitem.SetBitmap(icon)
+                    submenu.Append(menuitem)
 
 
     def OnMenuSelection(self, evt):
--- Utility.py
+++ Utility.py
@@ -1,5 +1,6 @@
 import wx
 import re
+import os
 
 def ColorDefault():
     return {
@@ -9,14 +10,24 @@ def ColorDefault():
     }
 
 Icons = {}
-def Icon(iconname):
-    if not Icons.get('iconname', None):
-        Icons[iconname] = wx.Bitmap(
-            wx.Image(
-                f"icons/{iconname}.png", wx.BITMAP_TYPE_ANY, -1,
+def Icon(name = '', powerset = '', power = ''):
+    # parse powerset and power to get name, if supplied
+    if powerset and power:
+        powerset = re.sub(r'\W+', '', powerset)
+        power    = re.sub(r'\W+', '', power)
+        name     = f"Powers/{powerset}_{power}"
+
+    if not Icons.get(name, None):
+        if os.path.exists(f"icons/{name}.png"):
+            Icons[name] = wx.Bitmap(
+                wx.Image(
+                    f"icons/{name}.png", wx.BITMAP_TYPE_ANY, -1,
+                )
             )
-        )
-    return Icons[iconname]
+        else:
+            print(f"Missing powers icon: {name}")
+
+    return Icons.get(name, None)
 
 def getMainKey(key):
     return re.sub('[LR]?(SHIFT|CTRL|ALT)\+?', '', key)

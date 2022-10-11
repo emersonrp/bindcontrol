f940cc41b52c682d5e14e55d004b54cd0eb1307f
Russell Pickett <emerson@hayseed.net>
2022-10-11 17:44:02 -0400

    icon_checker checks Incarnate icons;  remove six extra Incarnate icons

--- tools/icon_checker.py
+++ tools/icon_checker.py
@@ -11,11 +11,28 @@ sys.path.insert(0, parentdir)
 
 import GameData
 
+# from UI/IncarnateBox - TODO - put this in GameData or somewhere?
+Aliases = {
+    "Banished Pantheon"   : "Banished",
+    "Carnival of Shadows" : "Carnival",
+    "Cimerorans"          : "Cimeroran",
+    "Knives of Vengeance" : "Knives",
+    "Phantom"             : "Phantoms",
+    "Polar Lights"        : "Lights",
+    "Robotic Drones"      : "Drones",
+    "Storm Elementals"    : "Elementals",
+    "Talons of Vengeance" : "Talons",
+    "Warworks"            : "WarWorks",
+}
+
 count = 0
 filecheck = set()
 for filename in [f for f in os.listdir(f"{parentdir}/icons/Powers/") if f.endswith('png')]:
     filecheck.add(os.path.basename(filename))
+for filename in [f for f in os.listdir(f"{parentdir}/icons/Incarnate/") if f.endswith('png')]:
+    filecheck.add(os.path.basename(filename))
 
+# Archetype Primary and Secondary
 for archname, archdata in GameData.Archetypes.items():
     for category in ['Primary', 'Secondary', 'Epic']:
         for powerset, powers in archdata[category].items():
@@ -30,6 +47,7 @@ for archname, archdata in GameData.Archetypes.items():
                     # we found it, pull it back out of the set
                     if filename in filecheck: filecheck.remove(filename)
 
+# Pool Powers
 for poolname, pool in GameData.MiscPowers['Pool'].items():
     for power in pool:
 
@@ -42,6 +60,22 @@ for poolname, pool in GameData.MiscPowers['Pool'].items():
             # we found it, pull it back out of the set
             if filename in filecheck: filecheck.remove(filename)
 
+# Incarnate Powers
+for slot, slotdata in GameData.MiscPowers['Incarnate'].items():
+    for type in slotdata['Types']:
+        aliasedtype = Aliases.get(type, type)
+        for rarity in ['Common', 'Uncommon', 'Rare', 'VeryRare']:
+            filename = f"Incarnate_{slot}_{aliasedtype}_{rarity}.png"
+            if not os.path.exists(f"{parentdir}/icons/Incarnate/{filename}"):
+                print(f"Incarnate: {filename}")
+                count = count + 1
+            else:
+                if filename in filecheck: filecheck.remove(filename)
+
+
+
+
+## TODO - turn all of this into a proper test without "print" all over it
 if count: print(f"Total of {count} missing icons")
 
 if filecheck:

872d79b0ae446e8f8e8b6885d133db6f6bb06766
Russell Pickett <emerson@hayseed.net>
2022-10-11 16:15:35 -0400

    icon_checker checks Pool powers and for extra icons;  remove extra icons

--- tools/icon_checker.py
+++ tools/icon_checker.py
@@ -12,14 +12,40 @@ sys.path.insert(0, parentdir)
 import GameData
 
 count = 0
+filecheck = set()
+for filename in [f for f in os.listdir(f"{parentdir}/icons/Powers/") if f.endswith('png')]:
+    filecheck.add(os.path.basename(filename))
+
 for archname, archdata in GameData.Archetypes.items():
     for category in ['Primary', 'Secondary', 'Epic']:
         for powerset, powers in archdata[category].items():
             powerset = re.sub(r'\W+', '', powerset)
             for power in sorted(powers):
                 power = re.sub(r'\W+', '', power)
-                if not os.path.exists(f"{parentdir}/icons/Powers/{powerset}_{power}.png"):
-                    print(f"{archname}: {powerset}_{power}.png")
+                filename = f"{powerset}_{power}.png"
+                if not os.path.exists(f"{parentdir}/icons/Powers/{filename}"):
+                    print(f"{archname}: {filename}")
                     count = count + 1
+                else:
+                    # we found it, pull it back out of the set
+                    if filename in filecheck: filecheck.remove(filename)
 
-print(f"Total of {count} missing icons")
+for poolname, pool in GameData.MiscPowers['Pool'].items():
+    for power in pool:
+
+        power = re.sub(r'\W+', '', power)
+        filename = f"{poolname}_{power}.png"
+        if not os.path.exists(f"{parentdir}/icons/Powers/{filename}"):
+            print(f"Pool Power: {filename}")
+            count = count + 1
+        else:
+            # we found it, pull it back out of the set
+            if filename in filecheck: filecheck.remove(filename)
+
+if count: print(f"Total of {count} missing icons")
+
+if filecheck:
+    for fn in sorted(filecheck):
+        print(f"Extra icon file: {fn}")
+    print(f"Total of {len(filecheck)} extra icons")
+

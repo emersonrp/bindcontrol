519c6e65b906187b1326249ea1f315ef26c13595
Russell Pickett <emerson@hayseed.net>
2022-10-18 18:01:30 -0400

    Use PowerPicker for PowerBinder "use power" command

--- UI/PowerBinderDialog.py
+++ UI/PowerBinderDialog.py
@@ -625,7 +625,7 @@ class UsePowerCmd(PowerBindCmd):
         self.usePowerRBOff = wx.RadioButton(dialog, -1, "Off", style=wx.ALIGN_CENTER_VERTICAL)
         usePowerSizer.Add(self.usePowerRBOff, (0,3))
         usePowerSizer.Add(wx.StaticText(dialog, -1, "Power:"), (1,0), flag=wx.ALIGN_CENTER_VERTICAL)
-        self.usePowerName = wx.ComboBox(dialog, -1, choices=self.GetPowers())
+        self.usePowerName = PowerPicker(dialog)
         usePowerSizer.Add(self.usePowerName, (1,1), (1,3), flag=wx.EXPAND)
         usePowerSizer.AddGrowableCol(3)
 
@@ -644,11 +644,7 @@ class UsePowerCmd(PowerBindCmd):
             wx.LogError('PowerBindCmd "UsePowerCmd" got an impossible value for toggle/on/off')
             return ''
 
-        return f"{method} {self.usePowerName.GetValue()}"
-
-    def GetPowers(self):
-        profile = wx.Window.FindWindowByName("Profile")
-        return profile.General.GetPowers() or []
+        return f"{method} {self.usePowerName.GetLabel()}"
 
 ####### Use Power From Tray
 class UsePowerFromTrayCmd(PowerBindCmd):

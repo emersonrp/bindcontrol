9063e174840289318236ca1ce9f81be9e3044094
Russell Pickett <emerson@hayseed.net>
2022-10-28 17:40:54 -0400

    .gitignore ignoring MacOS debris;  build_mac.sh script v0.5

---
+++ build_mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pyinstaller \
+	--onefile \
+	--noconsole \
+	--noconfirm \
+	--clean \
+	--add-data icons:icons \
+	--add-data Help:Help \
+	--exclude-module _bz2 \
+	--exclude-module _ctypes \
+	--exclude-module _decimal \
+	--exclude-module _hashlib \
+	--exclude-module _lzma \
+	--exclude-module _socket \
+	--exclude-module _ssl \
+	BindControl.py

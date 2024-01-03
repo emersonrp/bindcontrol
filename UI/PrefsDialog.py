ecb7307677920d4808f60cb7f167df7447134bd8
Russell Pickett <emerson@hayseed.net>
2024-01-03 15:40:33 -0500

    Initial work to add controller page to prefs dialog.
    Still basically empty

--- UI/PrefsDialog.py
+++ UI/PrefsDialog.py
@@ -2,6 +2,7 @@ import wx
 import wx.lib.stattext as ST
 import UI
 from UI.KeySelectDialog import bcKeyButton
+from bcController import bcController
 
 class PrefsDialog(wx.Dialog):
     def __init__(self, parent):
@@ -9,70 +10,115 @@ class PrefsDialog(wx.Dialog):
 
         config = wx.ConfigBase.Get()
 
-        paddingSizer = wx.BoxSizer(wx.VERTICAL)
+        overallSizer = wx.BoxSizer(wx.VERTICAL)
 
-        sizer = wx.FlexGridSizer(2,0,0)
+        controller = bcController()
 
-        sizer.Add( wx.StaticText(self, label = 'Base Binds Directory:') , 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6 )
-        self.bindsDirPicker = wx.DirPickerCtrl(self, path = config.Read('BindPath'))
+        notebook = wx.Notebook(self)
+
+        ###
+        # GENERAL PANEL
+        ###
+        generalPanel = wx.Panel(notebook)
+        generalSizer = wx.FlexGridSizer(2,0,0)
+
+        generalSizer.Add( wx.StaticText(generalPanel, label = 'Base Binds Directory:') , 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6 )
+        self.bindsDirPicker = wx.DirPickerCtrl(generalPanel, path = config.Read('BindPath'))
         self.bindsDirPicker.SetToolTip('Bind files will be written to this folder, inside a profile-specific subfolder.  Keeping this path as short as possible is strongly recommended.')
-        sizer.Add( self.bindsDirPicker, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
+        generalSizer.Add( self.bindsDirPicker, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
 
         self.gameBindsDirPicker = None
         if (wx.Platform != '__WXMSW__'):
-            sizer.Add( wx.StaticText(self, label = "In-Game Binds Directory:") , 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
-            self.gameBindsDirPicker = wx.TextCtrl(self, value = config.Read('GameBindPath'))
+            generalSizer.Add( wx.StaticText(generalPanel, label = "In-Game Binds Directory:") , 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
+            self.gameBindsDirPicker = wx.TextCtrl(generalPanel, value = config.Read('GameBindPath'))
             self.gameBindsDirPicker.SetToolTip('When playing via Wine, the game\'s file paths will be different than the native ones.  Put a Windows path into this box that describes where Wine will find the above directory.  Keeping this path as short as possible is strongly recommended.')
-            sizer.Add( self.gameBindsDirPicker, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6 )
+            generalSizer.Add( self.gameBindsDirPicker, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6 )
 
-        sizer.Add( wx.StaticText(self, label = "Binds Reset Key:"), 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
-        self.ResetKey = bcKeyButton(self, -1, init ={ 'CtlName': 'ResetKey', })
+        generalSizer.Add( wx.StaticText(generalPanel, label = "Binds Reset Key:"), 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
+        self.ResetKey = bcKeyButton(generalPanel, -1, init ={ 'CtlName': 'ResetKey', })
         self.ResetKey.SetLabel( config.Read('ResetKey') )
         UI.Labels.update({ 'ResetKey': 'Binds Reset Key'})
-        sizer.Add( self.ResetKey, 1, wx.ALL|wx.ALIGN_CENTRE_VERTICAL, 6)
+        generalSizer.Add( self.ResetKey, 1, wx.ALL|wx.ALIGN_CENTRE_VERTICAL, 6)
 
 
-        splitKeyLabel = ST.GenStaticText(self, label = "Bind left and right modifier keys separately:")
-        sizer.Add( splitKeyLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6 )
-        self.UseSplitModKeys = wx.CheckBox(self)
+        splitKeyLabel = ST.GenStaticText(generalPanel, label = "Bind left and right modifier keys separately:")
+        generalSizer.Add( splitKeyLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6 )
+        self.UseSplitModKeys = wx.CheckBox(generalPanel)
         self.UseSplitModKeys.SetValue(config.ReadBool('UseSplitModKeys'))
         self.UseSplitModKeys.SetToolTip("By default, BindControl will bind modifier keys, eg CTRL and SHIFT, without regard to which side of the keyboard they're on.  Check this if you'd like to bind the left-side modifier keys separately from the right-side ones.")
-        sizer.Add( self.UseSplitModKeys, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6 )
+        generalSizer.Add( self.UseSplitModKeys, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6 )
 
         splitKeyLabel.CB = self.UseSplitModKeys
         splitKeyLabel.Bind( wx.EVT_LEFT_DOWN, self.onCBLabelClick )
 
 
-        flushBindsLabel = ST.GenStaticText(self, label = "Reset all binds to default before reapplying:")
-        sizer.Add( flushBindsLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6 )
-        self.FlushAllBinds = wx.CheckBox(self)
+        flushBindsLabel = ST.GenStaticText(generalPanel, label = "Reset all binds to default before reapplying:")
+        generalSizer.Add( flushBindsLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6 )
+        self.FlushAllBinds = wx.CheckBox(generalPanel)
         self.FlushAllBinds.SetValue(config.ReadBool('FlushAllBinds'))
         self.FlushAllBinds.SetToolTip("Set all binds to City of Heroes' default before applying BindControl's binds.  Uncheck this if you have added any binds into the game that are not managed by BindControl.")
-        sizer.Add( self.FlushAllBinds, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6 )
+        generalSizer.Add( self.FlushAllBinds, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6 )
 
         flushBindsLabel.CB = self.FlushAllBinds
         flushBindsLabel.Bind( wx.EVT_LEFT_DOWN, self.onCBLabelClick )
 
-        StartWithProfileLabel = wx.StaticText(self, label = "On startup, start with:")
-        sizer.Add( StartWithProfileLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
+        StartWithProfileLabel = wx.StaticText(generalPanel, label = "On startup, start with:")
+        generalSizer.Add( StartWithProfileLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
         StartWithSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.StartWithNewProfile  = wx.RadioButton(self, label="New Profile")
-        self.StartWithLastProfile = wx.RadioButton(self, label="Last Profile")
+        self.StartWithNewProfile  = wx.RadioButton(generalPanel, label="New Profile")
+        self.StartWithLastProfile = wx.RadioButton(generalPanel, label="Last Profile")
         if config.Read('StartWith') == "New Profile": self.StartWithNewProfile.SetValue(True)
         else:                                         self.StartWithLastProfile.SetValue(True)
         StartWithSizer.Add(self.StartWithNewProfile)
         StartWithSizer.Add(self.StartWithLastProfile)
-        sizer.Add (StartWithSizer, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
+        generalSizer.Add (StartWithSizer, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
+        generalPanel.SetSizerAndFit(generalSizer)
+
+        ###
+        # CONTROLLER PANEL
+        ###
+        controllerPanel = wx.Panel(notebook)
+        controllerSizer = wx.FlexGridSizer(2,0,0)
+
+        # Controller name display
+        controllerNameLabel = ST.GenStaticText(controllerPanel, label = 'Controller:')
+        controllerSizer.Add( controllerNameLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
+        controllerName = ST.GenStaticText(controllerPanel, label = 'No controller detected!')
+        if controller.GetNumberJoysticks() > 0:
+            controllerName.SetLabel(controller.GetProductName())
+        controllerSizer.Add( controllerName, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
+
+        # Pickers for controller_modifiers
+        controllerModsLabel = ST.GenStaticText(controllerPanel, label = 'Controller Modifiers:')
+        controllerSizer.Add( controllerModsLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
+        controllerModsButton = wx.Button(controllerPanel, label = "Controller Modifiers")
+        controllerSizer.Add(controllerModsButton, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
+
+        # Pickers for extra_modifiers
+        extraModsLabel = ST.GenStaticText(controllerPanel, label = 'Extra Modifiers:')
+        controllerSizer.Add( extraModsLabel, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 6)
+        extraModsButton = wx.Button(controllerPanel, label = "Extra Modifiers")
+        controllerSizer.Add(extraModsButton, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 6)
+
+        possible_mods = controller.ListOfPossibleMods()
+
+        controllerPanel.SetSizerAndFit(controllerSizer)
+
+        notebook.AddPage(generalPanel, "General", select = True)
+        notebook.AddPage(controllerPanel, "Controller")
 
         buttonSizer = wx.StdDialogButtonSizer()
         buttonSizer.AddButton(wx.Button(self, wx.ID_OK))
         buttonSizer.AddButton(wx.Button(self, wx.ID_CANCEL))
         buttonSizer.Realize()
 
-        paddingSizer.Add(sizer, 0, wx.ALL, 10)
+        paddingSizer = wx.BoxSizer(wx.VERTICAL)
         paddingSizer.Add(buttonSizer, 0, wx.ALL|wx.EXPAND, 10)
 
-        self.SetSizerAndFit(paddingSizer)
+        overallSizer.Add(notebook, 0, wx.ALL|wx.EXPAND)
+        overallSizer.Add(paddingSizer, 0, wx.ALL|wx.EXPAND)
+
+        self.SetSizerAndFit(overallSizer)
 
     def onCBLabelClick(self, evt):
         cblabel = evt.EventObject

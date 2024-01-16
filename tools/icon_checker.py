68bf6c9a1c623ce432176fe0a53a3e1b34880ea5
Russell Pickett <emerson@hayseed.net>
2024-01-16 06:37:00 -0600

    Rearrange top-level icons; update icon_checker

--- tools/icon_checker.py
+++ tools/icon_checker.py
@@ -5,9 +5,13 @@ import sys
 import inspect
 
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
+currframe = inspect.currentframe()
+if currframe:
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(currframe)))
+    parentdir = os.path.dirname(currentdir)
+    sys.path.insert(0, parentdir)
+else:
+    exit()
 
 import GameData
 
@@ -27,6 +31,10 @@ Aliases = {
 
 count = 0
 filecheck = set()
+for filename in [f for f in os.listdir(f"{parentdir}/icons/Archetypes/") if f.endswith('png')]:
+    filecheck.add(os.path.basename(filename))
+for filename in [f for f in os.listdir(f"{parentdir}/icons/Origins/") if f.endswith('png')]:
+    filecheck.add(os.path.basename(filename))
 for filename in [f for f in os.listdir(f"{parentdir}/icons/Powers/") if f.endswith('png')]:
     filecheck.add(os.path.basename(filename))
 for filename in [f for f in os.listdir(f"{parentdir}/icons/Incarnate/") if f.endswith('png')]:
@@ -34,8 +42,15 @@ for filename in [f for f in os.listdir(f"{parentdir}/icons/Incarnate/") if f.end
 for filename in [f for f in os.listdir(f"{parentdir}/icons/Inspirations/") if f.endswith('png')]:
     filecheck.add(os.path.basename(filename))
 
-# Archetype Primary and Secondary
+# Archetype, Primary, and Secondary
 for archname, archdata in GameData.Archetypes.items():
+    archfn = f"{archname}.png"
+    if not os.path.exists(f"{parentdir}/icons/Archetypes/{archfn}"):
+        print(f"{archfn}")
+        count = count + 1
+    else:
+        if archfn in filecheck: filecheck.remove(archfn)
+
     for category in ['Primary', 'Secondary', 'Epic']:
         for powerset, powers in archdata[category].items():
             powerset = re.sub(r'\W+', '', powerset)
@@ -47,6 +62,15 @@ for archname, archdata in GameData.Archetypes.items():
                     count = count + 1
                 else:
                     if filename in filecheck: filecheck.remove(filename)
+
+# Origins
+for origin in GameData.Origins:
+    filename = f"{origin}.png"
+    if not os.path.exists(f"{parentdir}/icons/Origins/{filename}"):
+        print(f"{filename}")
+        count = count + 1
+    else:
+        if filename in filecheck: filecheck.remove(filename)
 
 # Pool Powers
 for poolname, pool in GameData.MiscPowers['Pool'].items():
@@ -95,3 +119,5 @@ if filecheck:
         print(f"Extra icon file: {fn}")
     print(f"Total of {len(filecheck)} extra icons")
 
+if not (count or filecheck):
+    print("Icons are as expected.")

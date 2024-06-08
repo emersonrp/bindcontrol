3f9ea4f3f90ec64591a925396886d0fae7c5462d
Russell Pickett <emerson@hayseed.net>
2024-06-08 02:13:00 -0400

    Disable main scrollbars on Custom Binds page.

--- Profile.py
+++ Profile.py
@@ -46,7 +46,16 @@ class Profile(wx.Notebook):
         ]:
             self.Bind(evt, self.SetModified)
 
+        self.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self.onPageChanged)
+
         if loadfile: self.doLoadFromFile(loadfile)
+
+    def onPageChanged(self, evt):
+        scrollpane = self.Parent
+        if (evt.GetSelection() == 2): # TODO this is ugly hard-coded for "Custom Binds" tab
+            scrollpane.ShowScrollbars(wx.SHOW_SB_NEVER, wx.SHOW_SB_NEVER)
+        else:
+            scrollpane.ShowScrollbars(wx.SHOW_SB_DEFAULT, wx.SHOW_SB_DEFAULT)
 
     def CreatePage(self, module):
         module.BuildPage()

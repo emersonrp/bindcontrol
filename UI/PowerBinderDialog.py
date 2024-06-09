27149aaca4f6cdc027cc63de7f1238d2b1e8f701
Russell Pickett <emerson@hayseed.net>
2024-06-09 11:58:43 -0400

    Fix typo in "Use Power From Tray" PowerBinder picker

--- UI/PowerBinderDialog.py
+++ UI/PowerBinderDialog.py
@@ -1266,7 +1266,7 @@ class UsePowerFromTrayCmd(PowerBindCmd):
         usePowerFromTraySizer = wx.BoxSizer(wx.HORIZONTAL)
         usePowerFromTraySizer.Add(wx.StaticText(dialog, -1, "Tray:"), 0, wx.ALIGN_CENTER_VERTICAL|wx.RIGHT, 4)
         self.usePowerFromTrayTray = wx.Choice(dialog, -1,
-               choices = ['Main Tray', 'Alt Tray', 'Alt 2 Tray', 'Tray 1', 'Tray2', 'Tray 3',
+               choices = ['Main Tray', 'Alt Tray', 'Alt 2 Tray', 'Tray 1', 'Tray 2', 'Tray 3',
                    'Tray 4', 'Tray 5', 'Tray 6', 'Tray 7', 'Tray 8', 'Tray 9', 'Tray 10'])
         self.usePowerFromTrayTray.SetSelection(0)
         usePowerFromTraySizer.Add(self.usePowerFromTrayTray, 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)

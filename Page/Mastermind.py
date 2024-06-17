dfb56c3fea9d82c525de6ee5eb4163b1390e7efb
Russell Pickett <emerson@hayseed.net>
2024-06-16 22:33:06 -0400

    Add "find smallest substring" method for later use.

--- Page/Mastermind.py
+++ Page/Mastermind.py
@@ -1017,3 +1017,32 @@ class Mastermind(Page):
         'IncPetSize'                         : "Increase Pet Group Size",
         'DecPetSize'                         : "Decrease Pet Group Size",
     })
+
+# https://stackoverflow.com/questions/11245481/find-the-smallest-unique-substring-for-each-string-in-an-array
+def FindSmallestUniqueSubstring(names):
+    uniqueNames = [''] * len(names)
+    ### For each name
+    for nameInd, name in enumerate(names):
+        ### For each possible substring length
+        for windowSize in range(1,len(name)+1):
+            ### For each starting index of a substring
+            for substrInd in range(0, len(name)-windowSize+1):
+                substr = name[substrInd:substrInd+windowSize].lower()
+                foundMatch = False
+                ### For each other name
+                for otherNameInd in range(0, len(names)):
+                    if (nameInd != otherNameInd) and (names[otherNameInd].lower().find(substr) >= 0):
+                        foundMatch = True
+                        break
+
+                if not foundMatch:
+                    ### This substr works!
+                    uniqueNames[nameInd] = substr
+                    break
+            else:
+                # continue if the inner loop did not break
+                continue
+            # Inner loop broke, break again
+            break
+
+    return uniqueNames

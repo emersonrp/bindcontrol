198713a1c7ac6923384ccbdf78839703315d90b3
Russell Pickett <emerson@hayseed.net>
2024-06-28 01:34:55 -0400

    Allow multi-word names by using "" if whitespace detected

--- Page/Mastermind.py
+++ Page/Mastermind.py
@@ -385,15 +385,9 @@ class Mastermind(Page):
     def CheckUniqueNames(self):
         if (self.Profile.Archetype() == "Mastermind"):
             names = []
-            wasMultiWord = [False] * 6
             for i in (1,2,3,4,5,6):
                 ctrl = self.Ctrls[f'Pet{i}Name']
-                value = ctrl.GetValue()
-                # if we contain multiple words, use the longest
-                words = value.split()
-                if len(words) > 1:
-                    wasMultiWord[i-1] = True
-                    value = max(words, key = len)
+                value = ctrl.GetValue().strip() # don't allow whitespace at the ends
                 names.append(value)
 
             # we stash this away every time we calculate it so we can
@@ -404,8 +398,7 @@ class Mastermind(Page):
                 if self.uniqueNames[i-1]:
                     ctrl.RemoveError('unique')
                 else:
-                    MWWarning = "  BindControl only uses the longest word of a multi-word name." if wasMultiWord[i-1] else ""
-                    ctrl.AddError('unique', f'This pet name is not different enough to identify it uniquely.{MWWarning}')
+                    ctrl.AddError('unique', f'This pet name is not different enough to identify it uniquely.')
         else:
             for i in (1,2,3,4,5,6):
                 ctrl = self.Ctrls[f'Pet{i}Name']
@@ -778,6 +771,9 @@ def FindSmallestUniqueSubstring(names):
 
                 if not foundMatch:
                     ### This substr works!
+                    # If it contains a space, put "" around it
+                    if re.match(r'\s', substr):
+                        substr = f'"{substr}"'
                     uniqueNames[nameInd] = substr
                     break
             else:

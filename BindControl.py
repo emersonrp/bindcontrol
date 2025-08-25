b621fd0fc46271e7cdfb9f06cc27dce12171e72a
Russell Pickett <emerson@hayseed.net>
2025-08-24 22:22:08 -0400

    Add an initial Default Profile that will get saved in the config

--- BindControl.py
+++ BindControl.py
@@ -26,6 +26,7 @@ from UI.BindDirsWindow import BindDirsWindow
 from UI.PrefsDialog import PrefsDialog
 from Help import ShowHelpWindow
 from UI.ControlGroup import cgTextCtrl, cgButton
+from Util.DefaultProfile import DefaultProfile
 
 ###################
 # Main Window Class
@@ -76,6 +77,7 @@ class Main(wx.Frame):
         if not config.Exists('ShowDebugMessages')   : config.WriteBool('ShowDebugMessages', False)
         if not config.Exists('WinH')                : config.WriteInt('WinH', 1000)
         if not config.Exists('WinW')                : config.WriteInt('WinW', 1000)
+        if not config.Exists('DefaultProfile')      : config.Write('DefaultProfile', DefaultProfile)
 
         # migrate old "start with" preference.  Maybe remove this someday
         if config.Exists('StartWith'):

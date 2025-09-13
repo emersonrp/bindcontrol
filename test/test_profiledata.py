cf60300860c53c0411a580eeb396151701bf1201
Russell Pickett <emerson@hayseed.net>
2025-09-14 00:26:35 +0200

    More profiledata tests happening

--- test/test_profiledata.py
+++ test/test_profiledata.py
@@ -1,7 +1,10 @@
 #!/usr/sbin/python
 import wx
+import os
+import pytest
 import Models.ProfileData as ProfileData
 from unittest.mock import MagicMock
+from pathlib import Path
 
 def test_CheckProfileForBindsDir(tmp_path):
     (_, config) = doSetup(tmp_path)
@@ -50,7 +53,55 @@ def test_ProfilePath(tmp_path):
 
     config.DeleteAll()
 
+def test_ProfileData_init_neither(tmp_path):
+    (_, config) = doSetup(tmp_path)
 
+    with pytest.raises(Exception, match = 'neither filename nor newname'):
+        ProfileData.ProfileData(config)
+
+    config.DeleteAll()
+
+def test_ProfileData_init_newname(tmp_path):
+    (_, config) = doSetup(tmp_path)
+
+    pd = ProfileData.ProfileData(config, newname = 'test')
+    assert pd.Config   == config
+    assert pd.Filepath == ProfileData.ProfilePath(config) / 'test.bcp'
+    assert pd['ProfileBindsDir'] == 'test'
+    assert pd.Modified == True
+
+    config.DeleteAll()
+
+def test_ProfileData_init_filename(tmp_path):
+    (_, config) = doSetup(tmp_path)
+
+    profile_path = tmp_path / "testprofile.bcp"
+    with pytest.raises(Exception, match = f'whose file "{profile_path}" is missing'):
+        ProfileData.ProfileData(config, filename = str(profile_path))
+
+    profile_path.write_text('{}')
+    with pytest.raises(Exception, match = f'Something broke while loading'):
+        ProfileData.ProfileData(config, filename = str(profile_path))
+
+    profile_path.write_text('%#aflj BAD JSON BAD" $@!')
+    with pytest.raises(Exception, match = f'Something broke while loading'):
+        ProfileData.ProfileData(config, filename = str(profile_path))
+    profile_path.unlink()
+
+    fixtureprofile = Path(os.path.abspath(__file__)).parent / 'fixtures' / 'testprofile.bcp'
+    pd = ProfileData.ProfileData(config, filename = str(fixtureprofile))
+
+    assert pd.Filepath == fixtureprofile
+    assert pd.LastModTime == fixtureprofile.stat().st_mtime_ns
+    assert pd.Modified == False
+
+
+
+
+    config.DeleteAll()
+
+
+#########
 def doSetup(tmp_path):
     app = wx.App()
     config = wx.FileConfig()

a57ca08a89bd9eec588c04615a331b72f33d9e6d
Russell Pickett <emerson@hayseed.net>
2025-09-13 13:51:26 +0200

    Move MovementPowers' tObject into separate model;  write tests

---
+++ test/test_tObject.py
@@ -0,0 +1,34 @@
+from Models.tObject import tObject
+
+def test_togon():
+    t = tObject(FakeProfile('Homecoming'))
+    assert t.togon == 'powexectoggleon'
+    t = tObject(FakeProfile('Rebirth'))
+    assert t.togon == 'px_tgon'
+
+def test_togoff():
+    t = tObject(FakeProfile('Homecoming'))
+    assert t.togoff == 'powexectoggleoff'
+    t = tObject(FakeProfile('Rebirth'))
+    assert t.togoff == 'px_tgof'
+
+def test_KeyState():
+    t = tObject(FakeProfile('Homecoming'))
+    t.X = 1
+    t.S = 1
+    t.A = 1
+    assert t.KeyState() == '010110'
+    p = {'toggle' : 'S'}
+    assert t.KeyState(p) == '010010'
+
+def test_dirs():
+    t = tObject(FakeProfile('Homecoming'))
+    t.dow  = f'$$down 1'
+    t.forw = f'$$forw 1'
+    t.rig  = f'$$right 1'
+
+    assert t.dirs('UDFBLR') == "$$down 1$$forw 1$$right 1"
+
+class FakeProfile(object):
+    def __init__(self, server):
+        self.Server = server

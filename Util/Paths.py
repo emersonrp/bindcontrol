be4e961009c12e9fd0c78698fa7406671fe318ed
Russell Pickett <emerson@hayseed.net>
2025-09-20 10:03:48 +0200

    Tests for Util.Paths

--- Util/Paths.py
+++ Util/Paths.py
@@ -2,9 +2,11 @@ import sys, os
 from pathlib import Path
 
 # Things related to paths for the app
+#
 # class method to return the current Profile Path
 def ProfilePath(config): return Path(config.Read('ProfilePath'))
 
+# This is for finding things like loadable modules for PowerBinder, icons, etc
 # This relies on this file, Paths.py, staying exactly one level down, in /Util
 def GetRootDirPath():
     base_path = getattr(sys, '_MEIPASS', '')

497c771f5d79cf92a7bb178217094fe0d1f839ef
Russell Pickett <emerson@hayseed.net>
2025-09-21 14:35:58 +0200

    Go ahead and UpdateData every time we OK the PowerBinder dialog

--- UI/PowerBinder.py
+++ UI/PowerBinder.py
@@ -31,10 +31,9 @@ class PowerBinder(ErrorControlMixin, wx.TextCtrl):
         return PowerBinderDialog(self.DialogParent, self, extralength = self.ExtraLength)
 
     def UpdateState(self, bindString, state) -> None:
-        if bindString != self.GetValue():
-            self.CurrentState = state
-            self.SetValue(bindString)
-            wx.PostEvent(self, PowerBinderChanged(wx.NewId()))
+        self.CurrentState = state
+        self.SetValue(bindString)
+        wx.PostEvent(self, PowerBinderChanged(wx.NewId()))
 
 class PowerBinderDialog(wx.Dialog):
     def __init__(self, parent, powerbinder, extralength = 0) -> None:

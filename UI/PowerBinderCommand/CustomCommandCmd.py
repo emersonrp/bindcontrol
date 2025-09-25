13e29b6d3b3ee05cb444e6725242e97ed8ed951e
Russell Pickett <emerson@hayseed.net>
2025-09-24 22:14:47 -0400

    Add link to "List of Slash Commands" to "Custom Command" PowerBinder Command

--- UI/PowerBinderCommand/CustomCommandCmd.py
+++ UI/PowerBinderCommand/CustomCommandCmd.py
@@ -1,4 +1,6 @@
 import wx
+import wx.html
+import webbrowser
 from UI.PowerBinderCommand import PowerBinderCommand
 
 ####### Custom Bind
@@ -7,12 +9,25 @@ class CustomCommandCmd(PowerBinderCommand):
     # Menu = '' # This one gets treated specially, and should NOT define Menu
 
     def BuildUI(self, dialog):
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer = wx.BoxSizer(wx.VERTICAL)
         self.customBindName = wx.TextCtrl(dialog, -1)
         self.customBindName.SetHint('Custom Command Text')
-        sizer.Add(self.customBindName, 1, wx.ALIGN_CENTER_VERTICAL)
+        sizer.Add(self.customBindName, 0, wx.EXPAND)
+
+        link = wx.html.HtmlWindow(dialog, size = wx.Size(200,40))
+        dialog.SetBackgroundColour(link.GetHTMLBackgroundColour())
+        payload = 'https://wiki.cityofheroesrebirth.com/wiki/List_of_Slash_Commands' if self.Profile.Server == 'Rebirth' else 'https://homecoming.wiki/wiki/List_of_Slash_Commands'
+        link.SetPage(f'<html><body><center><a href="{payload}">List of Slash Commands</a></body></center></html>')
+        link.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.HandleLinkClicked)
+
+        sizer.Add(link, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.TOP, 10)
 
         return sizer
+
+    def HandleLinkClicked(self, evt):
+        linkinfo = evt.GetLinkInfo()
+        page = linkinfo.GetHref()
+        webbrowser.open(page)
 
     def MakeBindString(self):
         return self.customBindName.GetValue()

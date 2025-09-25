5fc4ae8a142d0252ac4adab24b7c77d34323d3e0
Russell Pickett <emerson@hayseed.net>
2025-09-25 14:45:07 -0400

    100% coverage on bindfile tests

--- test/test_bindfile.py
+++ test/test_bindfile.py
@@ -1,5 +1,7 @@
 from BindFile import KeyBind, BindFile
 from pathlib import Path
+import pytest
+import wx
 
 def test_keybind():
     kb = KeyBind('A', 'Test A', '', ['One', 'Two', 'Three'])
@@ -12,21 +14,67 @@ def test_keybind():
     kb3 = kb.MakeBind("$$unbind_all$$up 1$$emote wave")
     assert kb3.BindFileString() == 'A "unbind_all$$up 1$$emote wave"\n', "BindFileString correctly strips leading $$"
 
-def test_bindfile():
-    bf = BindFile('/tmp', 'c:\\tmp', Path('test', 'test'))
-
+def test_bindfile_keybinds(bindfile):
     kb = KeyBind('A', 'Test A', '', ['One', 'Two', 'Three'])
-    bf.SetBind(kb)
-    assert bf.KeyBinds['A'] == kb, "BindFile correctly sets KeyBinds from object"
+    bindfile.SetBind(kb)
+    assert bindfile.KeyBinds['A'] == kb, "BindFile correctly sets KeyBinds from object"
 
-    bf.SetBind('B', 'Test B', '', "This is a test")
-    assert isinstance(bf.KeyBinds['B'], KeyBind), "BindFile correctly creates KeyBind when SetBind called with strings"
+    bindfile.SetBind('B', 'Test B', '', "This is a test")
+    assert isinstance(bindfile.KeyBinds['B'], KeyBind), "BindFile correctly creates KeyBind when SetBind called with strings"
 
-    bf.Write()
-    written_file = Path('/tmp','test','test')
+def test_bindfile_write(bindfile, monkeypatch, tmp_path):
+    bindfile.SetBind('A', 'Test A', '', ['One', 'Two', 'Three'])
+    bindfile.SetBind('B', 'Test B', '', "This is a test")
+    bindfile.SetBind('SHIFT', 'Shift Test', '', "Testing bare SHIFT")
+
+    monkeypatch.setattr(Path, 'mkdir', raise_exception)
+    with pytest.raises(Exception, match = 'bindfile parent dirs'):
+        bindfile.Write()
+    monkeypatch.undo()
+
+    monkeypatch.setattr(Path, 'touch', raise_exception)
+    with pytest.raises(Exception, match = 'instantiate bindfile'):
+        bindfile.Write()
+    monkeypatch.undo()
+
+    monkeypatch.setattr(Path, 'write_text', raise_exception)
+    with pytest.raises(Exception, match = 'write to bindfile'):
+        bindfile.Write()
+    monkeypatch.undo()
+
+    bindfile.Write()
+
+    written_file = Path(tmp_path, 'test', 'test')
     assert written_file.exists(), "BindFile correctly writes a file"
-
-    contents = written_file.read_text()
-    assert contents == 'A "One$$Two$$Three"\nB "This is a test"\n', "BindFile's contents are as expected"
-
+    assert written_file.read_text() == 'A "One$$Two$$Three"\nB "This is a test"\nSHIFT "Testing bare SHIFT"\n', "BindFile's contents are as expected"
     written_file.unlink()
+
+    bindfile.SetBind('LONG', 'Long Bindstring', '', 300 * 'X')
+    with pytest.raises(Exception, match = 'badness in-game'):
+        bindfile.Write()
+
+def test_blf(config, bindfile, monkeypatch):
+    assert bindfile.BLF() == 'bindloadfilesilent c:\\tmp\\test\\test', 'BindFile.BLF returns correct path'
+
+    monkeypatch.setattr(config, 'ReadBool', lambda _: True)
+    assert bindfile.BLF() == 'bindloadfile c:\\tmp\\test\\test', 'BindFile.BLF honors "verbose blf" config'
+    monkeypatch.undo()
+
+#########
+def raise_exception(): raise(Exception)
+
+@pytest.fixture
+def config():
+    _ = wx.App()
+    config = wx.FileConfig()
+    wx.ConfigBase.Set(config)
+
+    yield config
+
+    config.DeleteAll()
+
+@pytest.fixture
+def bindfile(tmp_path):
+    bf = BindFile(tmp_path, 'c:\\tmp', Path('test', 'test'))
+
+    return bf

bfeeff552bd0cdffe2b108117c00ef3b109e4d57
Russell Pickett <emerson@hayseed.net>
2025-09-25 12:31:53 -0400

    Clean up BLF tests;  100% coverage on bcVersion tests

--- test/test_bcVersion.py
+++ test/test_bcVersion.py
@@ -1,18 +1,40 @@
 #!/usr/sbin/python
-import subprocess
-
+import os, shutil, subprocess
 import bcVersion
+import Util.Paths
 
-def git_found():
-    try:
-        subprocess.call(['git', '--version'], stdout=subprocess.DEVNULL)
-        return True
-    except:
-        return False
+def test_version_txt(monkeypatch, tmp_path):
+    monkeypatch.setattr(Util.Paths, 'GetRootDirPath', lambda: tmp_path)
+    version_txt = tmp_path / 'version.txt'
+    version_txt.write_text('test_version_1.1.1\n')
+    assert bcVersion.current_version() == 'test_version_1.1.1'
+    monkeypatch.undo()
 
-def test_get_git_tag():
-    if git_found():
-        assert bcVersion.get_git_tag() is not None
-    else:
-        assert bcVersion.get_git_tag() is None
+def test_get_git_tag(monkeypatch):
+    assert bcVersion.get_git_tag() != ''
+    assert bcVersion.current_version() is not None
+    monkeypatch.setattr(bcVersion, 'get_git_tag', lambda: None)
+    assert bcVersion.current_version() == "&lt; No version found &gt;"
+    monkeypatch.undo()
 
+def test_is_wsl():
+    assert bcVersion.is_wsl('not WSL thanks') == 0
+    assert bcVersion.is_wsl('fubble-Microsoft') == 1
+    assert bcVersion.is_wsl('wubble-microsoft-standard-WSL2') == 2
+
+def test_wsl_coverage(monkeypatch):
+    monkeypatch.setattr(bcVersion, 'wsl_available', lambda: True)
+    assert bcVersion.get_git_tag() != ''
+    monkeypatch.undo()
+
+    monkeypatch.setattr(bcVersion, 'do_git', lambda _,__: 1/0) # raises exception
+    assert bcVersion.get_git_tag() == ''
+
+    monkeypatch.setattr(os, 'name', 'nt')
+    monkeypatch.setattr(shutil, 'which', lambda _: True)
+    monkeypatch.setattr(subprocess, 'check_output', raise_subprocess_exception)
+    assert bcVersion.wsl_available() is False
+
+
+def raise_subprocess_exception(*_, **__):
+    raise(subprocess.SubprocessError)

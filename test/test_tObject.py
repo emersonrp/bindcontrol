73114346d33bd7f9f9752ad35380b15af8f0a676
Russell Pickett <emerson@hayseed.net>
2025-09-25 14:56:24 -0400

    100% coverage on Models/tObject

--- test/test_tObject.py
+++ test/test_tObject.py
@@ -1,4 +1,5 @@
 from Models.tObject import tObject
+import wx, pytest
 
 def test_togon():
     t = tObject(FakeProfile('Homecoming'))
@@ -29,9 +30,36 @@ def test_dirs():
 
     assert t.dirs('UDFBLR') == "$$down 1$$forw 1$$right 1"
 
+def test_blf(config):
+    t = tObject(FakeProfile('Homecoming'))
+    assert t.BLF('AJ') == "bindloadfilesilent c:\\TestPath\\AJ\\AJ000000.txt"
+
+    t.X = 1
+    t.S = 1
+    t.D = 1
+    assert t.BLF('sm') == "bindloadfilesilent c:\\TestPath\\SM\\SM010101.txt"
+
+    t.space = 1
+    t.W = 1
+    t.A = 1
+    assert t.BLF('sf', 's') == "bindloadfilesilent c:\\TestPath\\SF\\SF111111s.txt"
+
+#####
+@pytest.fixture
+def config():
+    _ = wx.App()
+    config = wx.FileConfig()
+
+    yield config
+
+    config.DeleteAll()
+
 class FakeProfile(object):
     def __init__(self, server):
         self.ServerName = server
 
     def Server(self):
         return self.ServerName
+
+    def BLF(self, *bits):
+        return "bindloadfilesilent c:\\TestPath\\" + "\\".join(bits)

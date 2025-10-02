e90ceeddef434622b2b8f223fe9946fcea5c4af4
Russell Pickett <emerson@hayseed.net>
2025-10-02 01:41:18 -0700

    Further work on Util/BuildFiles and tests.  I think it's working

--- test/test_utilbuildfiles.py
+++ test/test_utilbuildfiles.py
@@ -24,9 +24,9 @@ def test_ParseBuildFiles(tmp_path):
     assert data['Pool2'] == 'Fighting'
     assert data['Pool3'] == 'Leadership'
     assert data['Epic'] == 'Soul Mastery'
-    assert data['Fiery Aura'] == ['Fire Shield', 'Healing Flames', 'Blazing Aura', 'Plasma Shield', 'Burn', 'Temperature Protection']
-    assert data['War Mace'] == ['Bash', 'Pulverize', 'Taunt', 'Whirling Mace', 'Clobber', 'Crowd Control', 'Build Up']
-    assert data['Flight'] == ['Combat Flight', 'Fly', 'Fly Boost']
-    assert data['Fighting'] == ['Boxing', 'Tough', 'Weave']
-    assert data['Leadership'] == ['Defense', 'Tactics', 'Assault', 'Vengeance']
-    assert data['Soul Mastery'] == ['Gloom', 'Darkest Night']
+    assert data['PrimaryPowers'] == ['Fire Shield', 'Healing Flames', 'Blazing Aura', 'Plasma Shield', 'Burn', 'Temperature Protection']
+    assert data['SecondaryPowers'] == ['Bash', 'Pulverize', 'Taunt', 'Whirling Mace', 'Clobber', 'Crowd Control', 'Build Up']
+    assert data['Pool1Powers'] == ['Combat Flight', 'Fly', 'Fly Boost']
+    assert data['Pool2Powers'] == ['Boxing', 'Tough', 'Weave']
+    assert data['Pool3Powers'] == ['Defense', 'Tactics', 'Assault', 'Vengeance']
+    assert data['EpicPowers'] == ['Gloom', 'Darkest Night']

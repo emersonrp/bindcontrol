08cb085eff46e138d93d5452b85453deda0fd752
Russell Pickett <emerson@hayseed.net>
2025-10-02 01:29:22 -0700

    Make Util/BuildFiles extract powers;  pytest for same

--- test/test_utilbuildfiles.py
+++ test/test_utilbuildfiles.py
@@ -24,3 +24,9 @@ def test_ParseBuildFiles(tmp_path):
     assert data['Pool2'] == 'Fighting'
     assert data['Pool3'] == 'Leadership'
     assert data['Epic'] == 'Soul Mastery'
+    assert data['Fiery Aura'] == ['Fire Shield', 'Healing Flames', 'Blazing Aura', 'Plasma Shield', 'Burn', 'Temperature Protection']
+    assert data['War Mace'] == ['Bash', 'Pulverize', 'Taunt', 'Whirling Mace', 'Clobber', 'Crowd Control', 'Build Up']
+    assert data['Flight'] == ['Combat Flight', 'Fly', 'Fly Boost']
+    assert data['Fighting'] == ['Boxing', 'Tough', 'Weave']
+    assert data['Leadership'] == ['Defense', 'Tactics', 'Assault', 'Vengeance']
+    assert data['Soul Mastery'] == ['Gloom', 'Darkest Night']

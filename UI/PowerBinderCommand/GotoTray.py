2d0ec7882796385b423c93607f688831b95d27f0
Russell Pickett <emerson@hayseed.net>
2025-10-14 13:32:11 -0400

    Vertical-align controls in GotoTray PowerBinderCmd

--- UI/PowerBinderCommand/GotoTray.py
+++ UI/PowerBinderCommand/GotoTray.py
@@ -10,17 +10,17 @@ class GotoTray(PowerBinderCommand):
         CenteringSizer = wx.BoxSizer(wx.VERTICAL)
         MainSizer = wx.FlexGridSizer(2, 5, 5)
 
-        MainSizer.Add(wx.StaticText(dialog, label = "Set Hotbar:"), 0, wx.ALIGN_RIGHT)
+        MainSizer.Add(wx.StaticText(dialog, label = "Set Hotbar:"), 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
         choices = ['1', '2', '3', '4'] if self.Profile.Server() == "Rebirth" else ['1', '2', '3']
         self.HotbarPicker = wx.Choice(dialog, choices = choices)
         self.HotbarPicker.SetSelection(0)
-        MainSizer.Add(self.HotbarPicker, 1)
+        MainSizer.Add(self.HotbarPicker, 1, wx.ALIGN_CENTER_VERTICAL)
 
-        MainSizer.Add(wx.StaticText(dialog, label = "To Power Tray:"), 0, wx.ALIGN_RIGHT)
+        MainSizer.Add(wx.StaticText(dialog, label = "To Power Tray:"), 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
         choices = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         self.TrayPicker = wx.Choice(dialog, choices = choices)
         self.TrayPicker.SetSelection(0)
-        MainSizer.Add(self.TrayPicker, 1)
+        MainSizer.Add(self.TrayPicker, 1, wx.ALIGN_CENTER_VERTICAL)
 
         CenteringSizer.Add(MainSizer, flag = wx.ALIGN_CENTER)
         return CenteringSizer

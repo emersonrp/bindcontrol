c138d682f5643f2b2a13ed5255137260cbe57906
Russell Pickett <emerson@hayseed.net>
2025-10-16 22:55:16 -0400

    Fixup test broken by merge

--- test/test_profiledata.py
+++ test/test_profiledata.py
@@ -282,7 +282,7 @@ def test_GetDefaultProfileJSON(PD, config, DefaultProfile, monkeypatch):
 
     profiledata = json.loads(jsonstring)
     assert 'General' in profiledata
-    assert profiledata['ProfileBindsDir'] == 't2'
+    assert profiledata['ProfileBindsDir'] == 'nd'
 
 def test_BindsDirNotMine(monkeypatch, PD):
     # correctly returns False for unclaimed bindsdir

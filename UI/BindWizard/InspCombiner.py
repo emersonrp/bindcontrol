587036813d156a0989606056892e55cc987759ff
Russell Pickett <emerson@hayseed.net>
2026-03-19 11:13:54 -0400

    Initial work on InspCombiner Wizard.  Just getting started.

---
+++ UI/BindWizard/InspCombiner.py
@@ -0,0 +1,142 @@
+import re
+import wx
+from wx.adv import BitmapComboBox
+import GameData
+from Icon import GetIconBitmap
+import UI
+from UI.BindWizard import WizardParent
+from UI.KeySelectDialog import bcKeyButton, EVT_KEY_CHANGED
+
+class InspCombiner(WizardParent):
+    WizardName  = 'Inspiration Combiner'
+    WizToolTip  = 'Create a keybind that, with multiple presses, will combine inspirations and optionally consume them'
+    WizHelpFile = 'InspCombiner.html'
+
+    def __init__(self, parent, init) -> None:
+        super().__init__(parent, init)
+
+    def BuildUI(self, dialog, init : dict|None = None) -> wx.Sizer:
+        init = init or {}
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.SetMinSize(wx.Size(700,-1))
+
+        typePickerSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.TypePicker = BitmapComboBox(dialog, style = wx.CB_READONLY)
+        for insptype, info in GameData.Inspirations['Single'].items():
+            baseicon = GetIconBitmap('Inspirations', info['tiers'][0])
+            self.TypePicker.Append(insptype, baseicon)
+        self.TypePicker.SetSelection(0)
+
+        typePickerSizer.Add(wx.StaticText(dialog, label = 'Inspiration Type:', style=wx.ALIGN_RIGHT), 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+        typePickerSizer.Add(self.TypePicker, 1, wx.ALL, 5)
+
+        mainSizer.Add(typePickerSizer, 0, wx.EXPAND|wx.ALL, 10)
+
+        return mainSizer
+
+    def Serialize(self):
+        return {
+            'IncData' : self.State.get('WizData', {}).get('IncData', {}),
+            'BindKey' : self.BindKeyCtrl.Key,
+        }
+
+    def PaneContents(self):
+        bindpane = self.BindPane
+        panel = wx.Panel(bindpane.GetPane())
+        panel.Bind(wx.EVT_LEFT_DOWN, self.ShowWizard)
+        panelSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        listSizer = wx.BoxSizer(wx.HORIZONTAL)
+        # TODO actual pane contents go here once we figure them out
+        panelSizer.Add(listSizer, 1, wx.ALIGN_CENTER|wx.ALL, 15)
+
+        # And the bind key
+        bindkey = bindpane.Init.get('BindKey', '')
+        BindSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.BindKeyCtrl = bcKeyButton(panel, init = {
+            'CtlName' : bindpane.MakeCtrlName("BindKey"),
+            'Page'    : bindpane.Page,
+            'Key'     : bindkey,
+        })
+        self.BindKeyCtrl.Bind(EVT_KEY_CHANGED, self.OnKeyChanged)
+        BindSizer.Add(wx.StaticText(panel, label = "Bind Key:"), 0, wx.ALIGN_CENTER_VERTICAL|wx.LEFT|wx.RIGHT, 5)
+        BindSizer.Add(self.BindKeyCtrl, 0, wx.ALIGN_CENTER_VERTICAL)
+        bindpane.Ctrls[self.BindKeyCtrl.CtlName] = self.BindKeyCtrl
+        UI.Labels[self.BindKeyCtrl.CtlName] = f'Incarnate Set Bind "{bindpane.Title}"'
+
+        panelSizer.Add(BindSizer, wx.ALL, 10)
+
+        panel.SetSizer(panelSizer)
+
+        return panel
+
+    def OnKeyChanged(self, evt = None):
+        if evt: evt.Skip()
+
+    def UpdateState(self):
+        self.State = { 'WizData' : { } }
+
+    def CheckIfWellFormed(self) -> bool:
+        isWellFormed = True
+
+        bk = self.BindPane.GetCtrl('BindKey')
+        if not bk:
+            wx.LogError("BindKey missing from Ctrls in IncarnateSet.CheckIfWellFormed - this is a bug!")
+            isWellFormed = False
+
+        else:
+            if bk.Key:
+                bk.RemoveError('undef')
+            else:
+                bk.AddError('undef', 'The keybind has not been selected')
+                isWellFormed = False
+
+        return isWellFormed
+
+    def PopulateBindFiles(self):
+        profile = self.Profile
+
+        incdata = self.State.get('WizData', {}).get('IncData', {})
+        incarnate_lines = ['']
+        title = re.sub(r'\W+', '', self.BindPane.Title)
+        fake_blf = profile.BLF('wiz', f'{title}X')
+        extra_len = len(f"$$bindloadfilesilent {fake_blf}$$t $name, X of X, Press Again")
+        for slot, data in incdata.items():
+            if data['power'] == "Disable Slot": # pyright: ignore
+                incarnate_command = f'incarnateunequipbyslot {slot}'
+            else:
+                powername = re.sub(r' ', '_', data['power']) # pyright: ignore
+                incarnate_command = f'incarnate_equip {slot} {powername}'
+            if len(incarnate_lines[-1]) == 0:
+                incarnate_lines[-1] = incarnate_command
+            elif (len(incarnate_lines[-1]) + len(incarnate_command)) > (255 - extra_len):
+                incarnate_lines.append(incarnate_command)
+            else:
+                incarnate_lines[-1] = f'{incarnate_lines[-1]}$${incarnate_command}'
+
+        for i, line in enumerate(incarnate_lines):
+            bindfile = profile.GetBindFile('wiz', f'{title}{i}.txt')
+
+            if (i+1 < len(incarnate_lines)):
+                nextfile = i+1
+                line = line + f'$$t $name, {i+1} of {len(incarnate_lines)}, Press Again'
+            else:
+                nextfile = 0
+                line = line + '$$t $name, Set Loaded'
+
+            line = line + profile.BLF('wiz', f'{title}{nextfile}')
+            if i == 0: # start with the reset file, too
+                profile.ResetFile().SetBind(self.BindKeyCtrl.MakeBind(line))
+            bindfile.SetBind(self.BindKeyCtrl.MakeBind(line))
+
+    def AllBindFiles(self):
+        title = re.sub(r'\W+', '', self.BindPane.Title)
+
+        files = []
+
+        for i in range(6): # should not ever possibly be six steps, but....
+            files.append(self.Profile.GetBindFile('wiz', f'{title}{i}.txt'))
+        return {
+            'files' : files,
+            'dirs'  : [],
+        }

e95b139c491788afb733fe51318b2e6014962eeb
Russell Pickett <emerson@hayseed.net>
2026-04-04 19:19:09 -0400

    Fix to PowerPickerMenu not knowing about its Profile

--- UI/PowerPicker.py
+++ UI/PowerPicker.py
@@ -93,7 +93,7 @@ class PowerPicker(ProfileAwareControlMixin, ErrorControlMixin, wx.Button):
             self.IconFilename = iconfile
             self.SetIconFromFilename(iconfile)
 
-class PowerPickerMenu(FM.FlatMenu):
+class PowerPickerMenu(ProfileAwareControlMixin, FM.FlatMenu):
     def __init__(self, parent):
         super().__init__(parent)
         self.Bind(FM.EVT_FLAT_MENU_SELECTED, parent.OnMenuSelected)
@@ -105,85 +105,89 @@ class PowerPickerMenu(FM.FlatMenu):
         super().SetSize(size)
 
     def BuildMenu(self) -> None:
-        gen = self.Profile.General
+        if self.Profile:
+            gen = self.Profile.General
 
-        # primary / secondary / epic powers
-        archdata = GameData.Archetypes[gen.GetState('Archetype')]
-        for category in ['Primary', 'Secondary', 'Epic']:
-            submenu = PowerPickerMenu(self.Parent)
-            powerset = gen.GetState(category)
-            if not powerset: continue
-            self.AppendSubMenu(submenu, f"{category}: {powerset}")
+            # primary / secondary / epic powers
+            archdata = GameData.Archetypes[gen.GetState('Archetype')]
+            for category in ['Primary', 'Secondary', 'Epic']:
+                submenu = PowerPickerMenu(self.Parent)
+                powerset = gen.GetState(category)
+                if not powerset: continue
+                self.AppendSubMenu(submenu, f"{category}: {powerset}")
 
-            powers = archdata[category][powerset]
-            for power in powers:
-                if isinstance(power, dict):  # for sub sub menus like Corruptor -> Dual Pistols -> Swap Ammo
-                    [(subsubname, items)] = power.items()
-                    subsubmenu = PowerPickerMenu(self.Parent)
-                    for power in items:
+                powers = archdata[category][powerset]
+                for power in powers:
+                    if isinstance(power, dict):  # for sub sub menus like Corruptor -> Dual Pistols -> Swap Ammo
+                        [(subsubname, items)] = power.items()
+                        subsubmenu = PowerPickerMenu(self.Parent)
+                        for power in items:
+                            if not self.ShowPower(category, power): continue
+                            icon = GetIcon('Powers', f'{powerset}_{power}')
+                            menuitem = PowerMenuItem(self, power, icon)
+                            subsubmenu.AppendItem(menuitem)
+                        subitem = submenu.AppendSubMenu(subsubmenu, subsubname)
+                        # little bit of wx.Font hoop-jumping just for this corner case, sigh
+                        subitem.SetFont(UI.COHFont())
+                        icon = GetIcon('Powers', f'{powerset}_{subsubname}')
+                        if icon:
+                            subitem.SetNormalBitmap(icon.GetBitmap(wx.Size(32,32)))
+
+                    else:
                         if not self.ShowPower(category, power): continue
                         icon = GetIcon('Powers', f'{powerset}_{power}')
                         menuitem = PowerMenuItem(self, power, icon)
-                        subsubmenu.AppendItem(menuitem)
-                    subitem = submenu.AppendSubMenu(subsubmenu, subsubname)
-                    # little bit of wx.Font hoop-jumping just for this corner case, sigh
-                    subitem.SetFont(UI.COHFont())
-                    icon = GetIcon('Powers', f'{powerset}_{subsubname}')
-                    if icon:
-                        subitem.SetNormalBitmap(icon.GetBitmap(wx.Size(32,32)))
+                        submenu.AppendItem(menuitem)
 
-                else:
-                    if not self.ShowPower(category, power): continue
-                    icon = GetIcon('Powers', f'{powerset}_{power}')
-                    menuitem = PowerMenuItem(self, power, icon)
-                    submenu.AppendItem(menuitem)
+            # Pool powers
+            for poolpicker in ["Pool1", "Pool2", "Pool3", "Pool4"]:
+                poolname = gen.GetState(poolpicker)
+                if poolname:
+                    submenu = PowerPickerMenu(self.Parent)
+                    self.AppendSubMenu(submenu, "Pool: " + poolname)
 
-        # Pool powers
-        for poolpicker in ["Pool1", "Pool2", "Pool3", "Pool4"]:
-            poolname = gen.GetState(poolpicker)
-            if poolname:
-                submenu = PowerPickerMenu(self.Parent)
-                self.AppendSubMenu(submenu, "Pool: " + poolname)
+                    powers = GameData.PoolPowers[poolname]
+                    for power in powers:
+                        if not self.ShowPower(poolpicker, power): continue
+                        icon = GetIcon('Powers', f'{poolname}_{power}')
+                        menuitem = PowerMenuItem(self, power, icon)
+                        submenu.AppendItem(menuitem)
 
-                powers = GameData.PoolPowers[poolname]
-                for power in powers:
-                    if not self.ShowPower(poolpicker, power): continue
-                    icon = GetIcon('Powers', f'{poolname}_{power}')
-                    menuitem = PowerMenuItem(self, power, icon)
-                    submenu.AppendItem(menuitem)
+            # Incarnate Powers
+            menu = PowerPickerMenu(self.Parent)
+            for slot, slotdata in GameData.IncarnatePowers.items():
+                if 'Powers' in slotdata:
+                    submenu = PowerPickerMenu(self.Parent)
+                    menu.AppendSubMenu(submenu, slot)
+                    for inc_type in slotdata['Types']:
+                        subsubmenu = PowerPickerMenu(self.Parent)
+                        submenu.AppendSubMenu(subsubmenu, inc_type)
 
-        # Incarnate Powers
-        menu = PowerPickerMenu(self.Parent)
-        for slot, slotdata in GameData.IncarnatePowers.items():
-            if 'Powers' in slotdata:
-                submenu = PowerPickerMenu(self.Parent)
-                menu.AppendSubMenu(submenu, slot)
-                for inc_type in slotdata['Types']:
-                    subsubmenu = PowerPickerMenu(self.Parent)
-                    submenu.AppendSubMenu(subsubmenu, inc_type)
+                        for index, power in enumerate(slotdata['Powers']):
+                            rarity = Rarities[ index ]
 
-                    for index, power in enumerate(slotdata['Powers']):
-                        rarity = Rarities[ index ]
+                            # aliases for the Lore types ie "Polar Lights" => "Lights" to match the icons
+                            aliasedtype = Aliases.get(inc_type, inc_type)
 
-                        # aliases for the Lore types ie "Polar Lights" => "Lights" to match the icons
-                        aliasedtype = Aliases.get(inc_type, inc_type)
+                            icon = GetIcon('Incarnate', f'{slot}_{aliasedtype}_{rarity}')
+                            menuitem = PowerMenuItem(self, f"{inc_type} {power}", icon)
+                            subsubmenu.AppendItem(menuitem)
+            self.AppendSubMenu(menu, "Incarnate")
 
-                        icon = GetIcon('Incarnate', f'{slot}_{aliasedtype}_{rarity}')
-                        menuitem = PowerMenuItem(self, f"{inc_type} {power}", icon)
-                        subsubmenu.AppendItem(menuitem)
-        self.AppendSubMenu(menu, "Incarnate")
-
-        # Misc Powers
-        submenu = self.MakeRecursiveMenu(GameData.MiscPowers)
-        self.AppendSubMenu(submenu, "Misc")
+            # Misc Powers
+            submenu = self.MakeRecursiveMenu(GameData.MiscPowers)
+            self.AppendSubMenu(submenu, "Misc")
 
     def ShowPower(self, category:str, power:str) -> bool:
         # This next line is uuuuugly.  What is the right way to do this?
         # Answer:  a method on General?  On Profile?
-        powerlist = self.Profile.General.Ctrls[f'{category}Powers'].GetValue()
-        if   powerlist == []    : return True # we haven't picked powers, show them all
-        elif power in powerlist : return True # we have picked this one, show it
-        else                    : return False
+        if self.Profile:
+            powerlist = self.Profile.General.Ctrls[f'{category}Powers'].GetValue()
+            if   powerlist == []    : return True # we haven't picked powers, show them all
+            elif power in powerlist : return True # we have picked this one, show it
+            else                    : return False
+        else:
+            return False
 
     def MakeRecursiveMenu(self, menustruct) -> FM.FlatMenu:
 

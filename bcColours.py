4bffc4e48c12fcfadd49fd366f1ded2c2ec61deb
Russell Pickett <emerson@hayseed.net>
2026-04-06 18:05:52 -0400

    Start to make keyselectdialog darkmode-aware

--- bcColours.py
+++ bcColours.py
@@ -1,10 +1,18 @@
 import wx
 
+# is there a saner way to do this?
+
 def DarkMode():
     return wx.SystemSettings.GetAppearance().IsDark()
 
 def ErrorColour() -> wx.Colour:
-    return wx.Colour(100, 40, 40) if DarkMode() else wx.Colour(255, 200, 200)
+    return wx.Colour(128, 0, 0) if DarkMode() else wx.Colour(255, 200, 200)
 
 def WarningColour() -> wx.Colour:
-    return wx.Colour(100, 100, 40) if DarkMode() else wx.Colour(255, 255, 200)
+    return wx.Colour(128, 128, 0) if DarkMode() else wx.Colour(255, 255, 200)
+
+def BlackColour() -> wx.Colour:
+    return wx.WHITE if DarkMode() else wx.BLACK
+
+def WhiteColour() -> wx.Colour:
+    return wx.BLACK if DarkMode() else wx.WHITE
